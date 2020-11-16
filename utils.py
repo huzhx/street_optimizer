7d46fe8d38901f32cbc379ad6aacee7404b2bde4
Zhaoxian Hu <huzhx@uci.edu>
2020-11-15 16:29:52 -0800

    add remove_num(addr) and its tests

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import re
+
 def shift_num(num):
     ''' Return a string
     if num < 10 make it 10
@@ -12,3 +14,11 @@ def shift_num(num):
     else:
         num = num // 10 * 10
     return str(num)
+
+def remove_num(addr):
+    ''' Return a string
+    if a number is concatenated with a string like xxxstr, leave the number since it is normally part of the street name. 
+    However, if a number appears alone or concatenated after a string like strxxx, remove the number
+    '''
+    addr = re.sub(r'([a-zA-Z ]+)(\d+)(\W|\s|$)', r'\1', addr.strip())
+    return addr.strip()
--- utils_test.py
+++ utils_test.py
@@ -1,4 +1,4 @@
-from utils import shift_num
+from utils import shift_num, remove_num
 
 def test_shift_num1():
     num = '1234'
@@ -17,3 +17,45 @@ def test_shift_num3():
     result = shift_num(num)
     expected = '10'
     assert result == expected
+
+def test_remove_num1():
+    addr = '1st ave'
+    result = remove_num(addr)
+    expected = '1st ave'
+    assert result == expected
+
+def test_remove_num2():
+    addr = '1st ave apt 123'
+    result = remove_num(addr)
+    expected = '1st ave apt'
+    assert result == expected
+
+def test_remove_num3():
+    addr = '1st ave apt123'
+    result = remove_num(addr)
+    expected = '1st ave apt'
+    assert result == expected
+
+def test_remove_num4():
+    addr = '1staveapt123'
+    result = remove_num(addr)
+    expected = '1staveapt'
+    assert result == expected
+
+def test_remove_num5():
+    addr = '1 st ave apt123'
+    result = remove_num(addr)
+    expected = '1 st ave apt'
+    assert result == expected
+
+def test_remove_num6():
+    addr = '1 st 3ave apt123'
+    result = remove_num(addr)
+    expected = '1 st 3ave apt'
+    assert result == expected
+
+def test_remove_num7():
+    addr = '1 st 3ave 123 apt'
+    result = remove_num(addr)
+    expected = '1 st 3ave apt'
+    assert result == expected 
