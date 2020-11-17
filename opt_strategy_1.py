0174a1309a1c971f89978e98184e9ccae44dd2a6
Zhaoxian Hu <huzhx@uci.edu>
2020-11-16 16:52:15 -0800

    update opt_strategy_1 to restore the original street number

--- opt_strategy_1.py
+++ opt_strategy_1.py
@@ -6,26 +6,32 @@ import copy
 class OptimizationStrategy1(OptimizationStrategy):
 
     def get_opt_address(self, org_address: Address):
+        ''' Return an address
+        - Send the masked address to https://geomap.ffiec.gov/FFIECGeocMap/GeocodeMap1.aspx to get an optimized address 
+        - If received an optimized address, restore the original street number 
+        - Else return None
+        '''
         masked_addr_wo_zip = self.mask_address(org_address).to_string_wo_zip()
         api_endpoint = 'https://geomap.ffiec.gov/FFIECGeocMap/GeocodeMap1.aspx/GetGeocodeData'
         payload = {'sSingleLine': masked_addr_wo_zip, 'iCensusYear': '2020'}
         resp = consume_api(api_endpoint, payload)
-        return resp['d']['sMatchAddr'] if resp['d']['sMsg'] == 'Match Found.' else 'Failed'
+        if (resp['d']['sMsg']== 'Match Found.'):
+            opt_address = Address(resp['d']['sAddress'], resp['d']['sCityName'], resp['d']['sStateAbbr'], resp['d']['sZipCode'])
+            org_street_num, _ = org_address.extract_street_num()
+            opt_address.replace_street_num(org_street_num)
+            return opt_address
+        else:
+            return None
 
     def mask_address(self, org_address: Address):
-        ''' Return a string
+        ''' Return an address
         Mask the address by the following algorithm:
-        - find the first number in the street and mask it
-        - for the rest of the string, remove the number in it if it is not part of the street name
+        - Find the first number in the street and mask it
+        - For the rest of the string, remove the number in it if it is not part of the street name
         '''
         org_street = org_address.street
-        first_num = ''
-        index = 0
-        while org_street[index].isdigit():
-            first_num += str(org_street[index])
-            index += 1
+        first_num, rest_street = org_address.extract_street_num()
         masked_num = shift_num(first_num)
-        rest_street = org_street[index:].strip()
         sanitized_street = remove_num(rest_street)
         masked_street = ' '.join([masked_num, sanitized_street])
         masked_address = copy.deepcopy(org_address)
--- opt_strategy_1_test.py
+++ opt_strategy_1_test.py
@@ -9,9 +9,16 @@ def test_mask_address():
     expected = '1230 Astor Ave, Ann Arbor, MI, 48105'
     assert result == expected
 
-def test_get_opt_address():
+def test_get_opt_address1():
     os1 = OptimizationStrategy1()
     address = Address('1234 Astor Ave', 'Ann Arbor','MI', '48105')
     result = os1.get_opt_address(address)
-    expected = '1230 ASTOR DR, ANN ARBOR, MI, 48104'
-    assert result.upper() == expected.upper()
+    expected = Address('1234 ASTOR DR', 'ANN ARBOR', 'MI', '48104')
+    assert result == expected
+
+def test_get_opt_address2():
+    os1 = OptimizationStrategy1()
+    address = Address('1234 A', 'Ann Arbor','MI', '48105')
+    result = os1.get_opt_address(address)
+    expected = None
+    assert result is None
