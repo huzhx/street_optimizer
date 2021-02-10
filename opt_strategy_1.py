62f2430236ab188aa790d63476aa152cf291faab
Zhaoxian Hu <huzhx@uci.edu>
2021-02-10 15:04:48 -0800

    update mask_address by implementing the new functions created in utils

--- opt_strategy_1.py
+++ opt_strategy_1.py
@@ -1,6 +1,6 @@
 from opt_strategy_abc import OptimizationStrategy
 from address import Address
-from utils import shift_num, remove_num, consume_api
+from utils import shift_num, remove_num, consume_api, remove_after_street_info, strip_apt_term, has_street_term, has_apt_term
 import copy
 
 class OptimizationStrategy1(OptimizationStrategy):
@@ -12,14 +12,17 @@ class OptimizationStrategy1(OptimizationStrategy):
         - Else return None
         '''
         masked_addr_wo_id_zip = self.mask_address(org_address).to_string_wo_id_zip()
-        print('{} -> {}'.format(org_address.to_string_wo_id_zip(), masked_addr_wo_id_zip))
+        print('Mask address:\n{} -> {}\n'.format(org_address.to_string_wo_id_zip(), masked_addr_wo_id_zip))
         api_endpoint = 'https://geomap.ffiec.gov/FFIECGeocMap/GeocodeMap1.aspx/GetGeocodeData'
         payload = {'sSingleLine': masked_addr_wo_id_zip, 'iCensusYear': '2020'}
         resp = consume_api(api_endpoint, payload)
         if (resp['d']['sMsg']== 'Match Found.'):
             opt_address = Address(org_address.id, resp['d']['sAddress'], resp['d']['sCityName'], resp['d']['sStateAbbr'], resp['d']['sZipCode'])
+            resp_opt_address_str = opt_address.to_string()
+            print('Received address: ', resp_opt_address_str)
             org_street_num, _ = org_address.extract_street_num()
             opt_address.replace_street_num(org_street_num)
+            print('Restore street number:\n{} -> {}\n'.format(resp_opt_address_str, opt_address.to_string()))
             return opt_address
         else:
             return None
@@ -29,11 +32,23 @@ class OptimizationStrategy1(OptimizationStrategy):
         Mask the address by the following algorithm:
         - Find the first number in the street and mask it
         - For the rest of the string, remove the number in it if it is not part of the street name
+        - Check if a street term exists
+            - If yes, remove all content after the street term
+            - If no, check if an apt term exists
+                - If yes, remove the apt info including the apt term
+                - If no, print out for manual review
         '''
         org_street = org_address.street
         first_num, rest_street = org_address.extract_street_num()
         masked_num = shift_num(first_num)
         sanitized_street = remove_num(rest_street)
+        if (has_street_term(sanitized_street)):
+            sanitized_street = remove_after_street_info(sanitized_street)
+        else:
+            if (has_apt_term(sanitized_street)):
+                sanitized_street = strip_apt_term(sanitized_street)
+            else:
+                print('\nNo street or apt term: ', sanitized_street)
         masked_street = ' '.join([masked_num, sanitized_street])
         masked_address = copy.deepcopy(org_address)
         masked_address.street = masked_street
--- opt_strategy_1_test.py
+++ opt_strategy_1_test.py
@@ -2,11 +2,39 @@ import pytest
 from opt_strategy_1 import OptimizationStrategy1
 from address import Address
 
-def test_mask_address():
+def test_mask_address1():
     os1 = OptimizationStrategy1()
     address = Address('1', '1234 Astor Ave', 'Ann Arbor','MI', '48105')
     result = os1.mask_address(address).to_string()
     expected = '1, 1230 Astor Ave, Ann Arbor, MI, 48105'
+    assert result == expected
+
+def test_mask_address2():
+    os1 = OptimizationStrategy1()
+    address = Address('1', '  1510 124th Street ', 'Los Angeles','CA', '90047')
+    result = os1.mask_address(address).to_string()
+    expected = '1, 1515 124Th Street, Los Angeles, CA, 90047'
+    assert result == expected
+
+def test_mask_address3():
+    os1 = OptimizationStrategy1()
+    address = Address('1', '1510 124th Street Apt 123', 'Los Angeles','CA', '90047')
+    result = os1.mask_address(address).to_string()
+    expected = '1, 1515 124Th Street, Los Angeles, CA, 90047'
+    assert result == expected
+
+def test_mask_address4():
+    os1 = OptimizationStrategy1()
+    address = Address('1', '1510 124th Street Apt123', 'Los Angeles','CA', '90047')
+    result = os1.mask_address(address).to_string()
+    expected = '1, 1515 124Th Street, Los Angeles, CA, 90047'
+    assert result == expected
+
+def test_mask_address5():
+    os1 = OptimizationStrategy1()
+    address = Address('1', '1510 124th 123', 'Los Angeles','CA', '90047')
+    result = os1.mask_address(address).to_string()
+    expected = '1, 1515 124Th, Los Angeles, CA, 90047'
     assert result == expected
 
 def test_get_opt_address1():
@@ -22,3 +50,17 @@ def test_get_opt_address2():
     result = os1.get_opt_address(address)
     expected = None
     assert result is None
+
+def test_get_opt_address3():
+    os1 = OptimizationStrategy1()
+    address = Address('1', '1510 124th Street', 'Los Angeles','CA', '90047')
+    result = os1.get_opt_address(address)
+    expected = Address('1', '1510 W 124TH ST', 'LOS ANGELES', 'CA', '90047')
+    assert result == expected
+
+def test_get_opt_address4():
+    os1 = OptimizationStrategy1()
+    address = Address('1', '1510 124th Street Apt 123', 'Los Angeles','CA', '90047')
+    result = os1.get_opt_address(address)
+    expected = Address('1', '1510 W 124TH ST', 'LOS ANGELES', 'CA', '90047')
+    assert result == expected
