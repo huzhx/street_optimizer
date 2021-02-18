91be6a02d5fb8d448603123a428253ce6a30f8ea
Zhaoxian Hu <huzhx@uci.edu>
2021-02-17 16:48:04 -0800

    rename get_problematic_addresses.py -> get_missing_street_apt_term_addresses.py

--- get_missing_street_apt_term_addresses.py
+++ get_missing_street_apt_term_addresses.py
@@ -9,11 +9,11 @@ def main(argv):
    try:
       opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
    except getopt.GetoptError:
-      print("get_problematic_addresses.py -i <inputfile> -o <outputfile>")
+      print("get_missing_street_apt_term_addresses.py -i <inputfile> -o <outputfile>")
       sys.exit(2)
    for opt, arg in opts:
       if opt == "-h":
-         print("get_problematic_addresses.py -i <inputfile> -o <outputfile>")
+         print("get_missing_street_apt_term_addresses.py -i <inputfile> -o <outputfile>")
          sys.exit()
       elif opt in ("-i", "--ifile"):
          inputfile = arg
@@ -23,7 +23,7 @@ def main(argv):
    print("Output file is ", outputfile)
    
    os1 = OptimizationStrategy1()
-   problematic_addresses = []
+   missing_street_apt_term_addresses = []
    fields = []
    with open(inputfile) as csvfile:
        csvreader = csv.reader(csvfile, delimiter=',')
@@ -31,12 +31,12 @@ def main(argv):
        for row in csvreader:
            address = Address(row[0], row[1], row[2], row[3], row[4])
            if (os1.missing_street_apt_term(address)):
-               problematic_addresses.append(row)
+               missing_street_apt_term_addresses.append(row)
     
    with open(outputfile, 'w') as csvfile:
        csvwriter = csv.writer(csvfile)
        csvwriter.writerow(fileds)
-       csvwriter.writerows(problematic_addresses)
+       csvwriter.writerows(missing_street_apt_term_addresses)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
